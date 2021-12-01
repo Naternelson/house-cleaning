313c0fcea7e92793541582abd677a659ddccb1a8
Nate <7nater.nelson@gmail.com>
2021-11-30 18:37:39 -0700

    Finallybasic email sent with env variables

---
+++ public/env.js
@@ -0,0 +1,5 @@
+window.env = {
+  "GMAIL_USER_TOKEN": "user_IOxJuE3ZbTTtLhYEnUzqp",
+  "GMAIL_TEMPLATE_ID": "template_vpu0hv6",
+  "GMAIL_SERVICE_ID": "service_x5sv3wf"
+};
--- src/components/action-form.js
+++ src/components/action-form.js
@@ -1,9 +1,32 @@
 import React from "react";
-import { TextField , Box} from "@mui/material";
+import { TextField , Box, Button} from "@mui/material";
+import env from 'react-dotenv'
+import emailjs from 'emailjs-com'
 
 export default function ActionForm(){
+
+    const submitHandler = (e)=>{
+        e.preventDefault()
+        // const params = [userEmailToken]
+        const params = [env.GMAIL_SERVICE_ID, env.GMAIL_TEMPLATE_ID, {message: 'Hello World'}, env.GMAIL_USER_TOKEN]
+        console.log(params)
+        emailjs.send(...params)
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.error(error.text);
+            });
+    }
+
+
     return (
-        <Box component="form">
+        <Box 
+            component="form"
+            onSubmit={submitHandler}
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+        >
             <div>
                 <TextField
                     id="first-name"
@@ -22,7 +45,7 @@ export default function ActionForm(){
                 />
                 <TextField
                     id="phone"
-                    label="phone"
+                    label="Phone"
                 />
             </div>
             <div>
@@ -36,6 +59,9 @@ export default function ActionForm(){
                     type="search"
                     label="State"
                 />
+            </div>
+            <div>
+                <Button variant="outlined" type="submit">Submit</Button>
             </div>
         </Box>
     )
